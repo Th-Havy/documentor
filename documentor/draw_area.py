c9359340d2c489cc018d7a3ea59ffeddf436701a
Th-Havy <thomashavy@hotmail.fr>
2023-08-12 12:44:24 +0200

    basic rectangle drawing implemented

--- documentor/draw_area.py
+++ documentor/draw_area.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QPoint, QRect
 from PySide6.QtWidgets import QGraphicsView, QGraphicsScene
 from PySide6.QtGui import QPixmap
 
@@ -14,18 +14,29 @@ class DrawArea(QGraphicsView):
         self.scene = scene
         self.view = QGraphicsView(self.scene)
 
+        # To keep track of shape drawing
+        self.draw_begin = QPoint()
+        self.draw_end = QPoint()
+
+        self.draw_shapes()
+
+    def draw_shapes(self):
         ellipse = self.scene.addEllipse(10, 10, 100, 200, colors.pens[colors.Color.GREEN])
-        ellipse = self.scene.addEllipse(10, 10, 200, 200, colors.pens[colors.Color.RED])
         pixmap = QPixmap("C:/Users/Thomas/Desktop/plan.png")
         image = self.scene.addPixmap(pixmap)
 
     def mousePressEvent(self, event):
         if event.buttons() & Qt.MouseButton.LeftButton:
             print("Pressed")
+            self.draw_begin = event.pos()
+            self.draw_end = self.draw_begin
+            self.rectangle = self.scene.addRect(QRect(self.draw_begin, self.draw_end), colors.pens[colors.Color.RED])
 
     def mouseMoveEvent(self, event):
         if event.buttons() & Qt.MouseButton.LeftButton:
             print("Move")
+            self.draw_end = event.pos()
+            self.rectangle.setRect(QRect(self.draw_begin, self.draw_end))
 
     def mouseReleaseEvent(self, event):
         if event.button() & Qt.MouseButton.LeftButton:
